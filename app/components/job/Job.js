4304a1f05e80c55048ac4267032759277552e425
Adi <adij82@gmail.com>
2017-08-26 19:14:46 +0300

    first commit

---
+++ app/components/job/Job.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './job.css'
+
+export default class Job extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div className="job">
+        ldsldlsdls
+      </div>
+
+    )
+  }
+}
---
+++ test.js
@@ -0,0 +1,21 @@
+
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date(1503595750466);
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  console.log( h + ":" + m + ":" + s);
+
+}
+startTime();
